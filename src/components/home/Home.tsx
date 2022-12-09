af3ccc02d1d7c8029092ed9c409c7fc8823bc424
Terrance Prasath <terranceprasath97@gmail.com>
2022-12-10 02:27:43 +0800

    refactor home component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,22 +1,12 @@
 import React from "react";
-import { useLocalStorage } from "../../hooks/sessionStorage";
-import { firstRowTimeZone, secondRowTimeZone, thirdRowTimeZone } from "../../mocks/TimeZone";
-import ClockList, { ClockListRowType } from "../ClockList/ClockList";
+import { useGlobalSettingContext } from "../../context/GlobalSettingContext";
+import ClockList from "../ClockList/ClockList";
 import "./home.scss";
 
 export const Home = () => {
-  const [list, setList] = React.useState<ClockListRowType>();
-  const { sessionData, storeData } = useLocalStorage("preference-clock-list", true);
+  const { defaultHomeTimezone } = useGlobalSettingContext();
 
-  React.useEffect(() => {
-    if (sessionData) {
-      setList(sessionData as ClockListRowType);
-      return;
-    }
-    setList({ firstRow: firstRowTimeZone, secondRow: secondRowTimeZone, thirdRow: thirdRowTimeZone });
-  }, [sessionData]);
-
-  return <ClockList list={list} />;
+  return <ClockList list={defaultHomeTimezone} />;
 };
 
 export default Home;
